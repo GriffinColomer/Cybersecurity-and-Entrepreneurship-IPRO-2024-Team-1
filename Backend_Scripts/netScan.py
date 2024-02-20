a39f7da6a34ed8a298cbeb7b82de7e859789337c
dhalmy <91496056+dhalmy@users.noreply.github.com>
2024-02-19 19:39:53 -0600

    updated

--- Backend_Scripts/netScan.py
+++ Backend_Scripts/netScan.py
@@ -50,7 +50,7 @@ def main():
     json_IP = json.dumps(local_IP, indent = 3)
 
     # Writes json to a file
-    with open("loaclIP.json", "w") as output:
+    with open("localIP.json", "w") as output:
         output.write(json_IP)
 
 if __name__ == '__main__':
