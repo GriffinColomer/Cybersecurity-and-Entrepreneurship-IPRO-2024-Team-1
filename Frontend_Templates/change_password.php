c37aaefbb8e98ad71d6aa10e7ce3d22cac9072a1
Griffin Colomer <68077458+GriffinColomer@users.noreply.github.com>
2024-03-25 19:08:37 -0500

    Update change_password.php
    have the IP getting inputed from the json

--- Frontend_Templates/change_password.php
+++ Frontend_Templates/change_password.php
@@ -3,7 +3,8 @@
 $requestBody = file_get_contents('php://input');
 $requestData = json_decode($requestBody, true);
 $deviceMAC = isset($requestData['MAC']) ? $requestData['MAC'] : '';
+$deviceIP = isset($requestData['IP']) ? $requestData['IP'] : '';
 $devicePassword = isset($requestData['password']) ? $requestData['password'] : '';
-$output = shell_exec("sudo python ../Backend_Scripts/password_controller.py $deviceMAC $devicePassword");
+$output = shell_exec("sudo python ../Backend_Scripts/password_controller.py $deviceMAC $deviceIP $devicePassword");
 
 ?>
